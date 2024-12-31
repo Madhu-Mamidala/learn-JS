0f1e84b7547b4b3229429752b43efd49ff1fc12e
Madhu-Mamidala <mamidala.madhu5s@gamil.com>
2024-12-31 16:05:31 +0530

    added projects

---
+++ dataTypes/DataTypes.js
@@ -0,0 +1,36 @@
+"use strict";
+// Number 
+const num = 2025;
+console.log("Type Of : ",typeof num,",","num : ",num)
+
+// String
+
+const str = "Madhu";
+console.log("Type Of : ",typeof str,",","str : ",str)
+
+
+// Boolean
+
+const Boolean = true;
+console.log("Type Of : ",typeof Boolean,",","value : ",Boolean)
+
+
+//undefined
+
+let undefinedVar;
+console.log("Type Of : ",typeof undefinedVar,",","undefinedVar : ",undefinedVar)
+
+// Null
+
+let nullVar = null;
+console.log("Type Of : ",typeof nullVar,",","nullVar : ",nullVar)
+
+// Symbol
+
+const symbolVar = Symbol("symbol");
+console.log("Type Of : ",typeof symbolVar,",","symbolVar : ",symbolVar)
+
+// BigInt
+
+const bigIntVar = 9007199254740991n;
+console.log("Type Of : ",typeof bigIntVar,",","bigIntVar : ",bigIntVar)
